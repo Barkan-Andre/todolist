6d7eec326427deef8605178b4b258dc0ae694c00
NIK <nikita1z@mail.ru>
2023-08-16 21:36:08 +0300

    password_new

--- src/core/views.py
+++ src/core/views.py
@@ -1,32 +1,42 @@
-from django.contrib.auth import authenticate, login
-from rest_framework import generics, status
-from rest_framework.generics import CreateAPIView
+from django.contrib.auth import login, logout
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
-from core.serializers import CreateUserSerializer, LoginSerializer
+from core.models import User
+from core.serializers import (CreateUserSerializer, LoginSerializer,
+                              ProfileSerializer, UpdatePasswordSerializer)
 
 
 class SignupView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
 
 
-class LoginView(CreateAPIView):
-    """Войти"""
+class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login(request=request, user=serializer.save())
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        if not serializer.is_valid():
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(request, username=username, password=password)
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
-        # login
-        if user:
-            login(request, user)
-            return Response(status=status.HTTP_200_OK)
+    def get_object(self) -> User:
+        return self.request.user
 
-        return Response(data={'password': ['Invalid password']}, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UpdatePasswordSerializer
+
+    def get_object(self):
+        return self.request.user
